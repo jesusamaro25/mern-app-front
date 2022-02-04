72870e0d9f44c61c49802f9666c8dca8a156699a
Jesus Amaro <jesusamaro1995@gmail.com>
2022-02-04 16:02:55 -0400

    modal design

---
+++ src/components/calendar/CalendarModal.jsx
@@ -0,0 +1,153 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import DateTimePicker from 'react-datetime-picker';
+import moment from 'moment'
+import Swal from 'sweetalert2';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
+Modal.setAppElement('#root');
+
+const now = moment().minutes(0).seconds(0).add(1, 'hours');
+const finish = moment().minutes(0).seconds(0).add(2, 'hours');
+
+export const CalendarModal = () => {
+
+    const [formValues, setFormValues] = useState({
+        title: 'evento',
+        notes: '',
+        start: now.toDate(),
+        end: finish.toDate()
+    });
+
+    const {notes, title, start, end} = formValues;
+
+    const [dateStart, setDateStart] = useState(now.toDate());
+    const [dateEnd, setDateEnd] = useState(finish.toDate());
+    const [titleValid, setTitleValid] = useState(true);
+
+    const closeModal = () => {
+        console.log('first');
+    }
+
+    const handleStartDateChange = (e) => {
+        setDateStart(e)
+        setFormValues({
+            ...formValues,
+            start: e
+        })
+    }
+
+    const handleEndDateChange = (e) => {
+        setDateEnd(e)
+        setFormValues({
+            ...formValues,
+            end: e
+        })
+    }
+
+    const handleInputChange = ({target}) => {
+        setFormValues({
+            ...formValues,
+            [target.name]: target.value
+        })
+    }
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+
+        const momentStart = moment(start)
+        const momentEnd = moment(end)
+
+        if (momentStart.isSameOrAfter(momentEnd)) {
+            return Swal.fire('Error', 'la fecha fin debe ser mayor a la inicial', 'error')
+        }
+
+        if (title.trim().length < 2) {
+            return setTitleValid(false)
+        }
+
+        setTitleValid(true)
+        closeModal();
+    }
+
+    return (
+        <Modal
+            isOpen={true}
+            onRequestClose={closeModal}
+            style={customStyles}
+            className="modal"
+            overlayClassName="modal-fondo"
+            closeTimeoutMS={200}
+        >
+            <h1> Nuevo evento </h1>
+            <hr />
+            <form className="container" onSubmit={handleSubmitForm}>
+
+                <div className="form-group">
+                    <label>Fecha y hora inicio</label>
+                    <DateTimePicker
+                        onChange={handleStartDateChange}
+                        value={dateStart}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Fecha y hora fin</label>
+                    <DateTimePicker
+                        minDate={dateStart}
+                        onChange={handleEndDateChange}
+                        value={dateEnd}
+                    />
+                </div>
+
+                <hr />
+                <div className="form-group">
+                    <label>Titulo y notas</label>
+                    <input
+                        type="text"
+                        className= {`form-control 
+                        ${!titleValid && 'is-invalid'}`}
+                        placeholder="Título del evento"
+                        name="title"
+                        autoComplete="off"
+                        value={title}
+                        onChange={handleInputChange}
+                    />
+                    <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+                </div>
+
+                <div className="form-group">
+                    <textarea
+                        type="text"
+                        className="form-control"
+                        placeholder="Notas"
+                        rows="5"
+                        name="notes"
+                        value={notes}
+                        onChange={handleInputChange}
+                    ></textarea>
+                    <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary btn-block"
+                >
+                    <i className="far fa-save"></i>
+                    <span> Guardar</span>
+                </button>
+
+            </form>
+        </Modal>
+    );
+};
--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,17 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavBar } from '../ui/NavBar';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+import 'moment/locale/es';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { messages } from '../../helpers/calendar-messages';
+import { CalendarEvent } from './CalendarEvent';
+import { CalendarModal } from './CalendarModal';
+
 const localizer = momentLocalizer(moment)
 const events = [{
     title: 'cumple',
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    notes: 'play',
+    user: {
+      _id: '1',
+      name: 'jesus'
+    }
 }]
 
 export const CalendarScreen = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+
+  const onDoubleClick = (e) => {
+    console.log(e);
+  }
+
+  const onSelectEvent = (e) => {
+    console.log(e);
+  }
+
+  const onViewChange = (e) => {
+    setLastView(e) //guardamos la vista, si estamos en mes y recargo, entonces quedo en mes
+    localStorage.setItem('lastView',e)
+  }
+
+const eventStyleGetter = (event, start, end, isSelected) => {
+  const style = {
+    backgroundColor: '#367CF7',
+    borderRadius: '0px',
+    opacity: 0.8,
+    display: 'block',
+    color: 'white'
+  }
+  return {
+    style
+  }
+}
+
   return (
   <div>
     <NavBar/>
@@ -21,8 +60,18 @@ export const CalendarScreen = () => {
       events={events}
       startAccessor="start"
       endAccessor="end"
-      style={{ height: 500 }}
+      messages={messages}
+      eventPropGetter={eventStyleGetter}
+      onDoubleClickEvent={onDoubleClick}
+      onSelectEvent={onSelectEvent}
+      onView={onViewChange}
+      view={lastView}
+      components={{
+        event: CalendarEvent
+      }
+      }
     />
+    <CalendarModal />
   </div>
   );
 };
